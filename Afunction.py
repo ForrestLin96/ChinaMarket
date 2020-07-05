1906f1d9c3f7082801c7832cd5ea928c79d5e893
ForrestLin96 <63614850+ForrestLin96@users.noreply.github.com>
2020-07-05 23:23:41 +0800

    Add files via upload

---
+++ Afunction.py
@@ -0,0 +1,104 @@
+import pandas as pd
+import datetime
+import numpy as np
+from array import *
+import pandas_datareader.data as web
+from pandas import Series, DataFrame
+import matplotlib.pyplot as plt
+import matplotlib
+from sklearn.model_selection import cross_val_score,cross_val_predict
+from sklearn.metrics import precision_recall_curve
+matplotlib.style.use('ggplot')
+
+def plot_precision_recall_vs_threshold(index,stock,method_list,precisions, recalls, thresholds):
+    plt.plot(thresholds, precisions[:-1], "b--", label="Precision")
+    plt.plot(thresholds, recalls[:-1], "g-", label="Recall")
+    plt.xlabel("Threshold")
+    plt.legend(loc="lower left")
+    plt.xlim([0.5, 0.95])
+    plt.ylim([0, 1])
+    plt.title(stock+'\n'+method_list.columns[index])
+#Define Stochastic Osciliator
+def calculate_k(ticker_df,cycle, M1 ):
+    close = ticker_df['close']
+    highest_hi = ticker_df['high'].rolling(window = cycle).max()
+    lowest_lo = ticker_df['low'].rolling(window=10).min()
+    ticker_df['rsv'] = (close - lowest_lo)/(highest_hi - lowest_lo)*100
+    ticker_df['K'] = ticker_df['rsv'].rolling(window=M1).mean()
+    ticker_df['K']  =  ticker_df['K'] .fillna(50)
+    ticker_df['K_diff'] = ticker_df['K'].diff()
+    ticker_df['K_prev'] = ticker_df['K'] - ticker_df['K_diff']
+    ticker_df['K_ROC'] = ticker_df['K']/ticker_df['K_prev']
+    return ticker_df
+def calculate_dj(ticker_df, M2 ):
+    ticker_df['D'] = ticker_df['K'].rolling(window = M2).mean()
+    ticker_df['D'] = ticker_df['D'].fillna(50)
+    ticker_df['D_diff'] = ticker_df['D'].diff()
+    ticker_df['D_prev'] = ticker_df['D'] - ticker_df['D_diff']
+    ticker_df['D_ROC'] = ticker_df['D']/ticker_df['D_prev']
+    ticker_df['J'] = M2*ticker_df['K']-(M2-1)*ticker_df['D']
+    ticker_df['J_diff'] = ticker_df['J'].diff()
+    ticker_df['J_prev'] = ticker_df['J'] - ticker_df['J_diff']
+    ticker_df['J_ROC'] = ticker_df['J']/ticker_df['J_prev']
+    return ticker_df
+def stochastic_oscillator(ticker_df,cycle=12, M1=4, M2= 3):
+    ticker_df = calculate_k(ticker_df,cycle,M1)
+    ticker_df = calculate_dj(ticker_df, M2)
+    return ticker_df
+#Evenly separeate all days into good selling points and poor selling points
+
+def selljudge(df,cycle=10,duration=-180):
+    #ticker_df['Max'] = ticker_df['close'].rolling(window = cycle).max().shift(-cycle)
+    df['Good sell Point?'] =0
+    df['10天最低价'] =df['close'].rolling(window =cycle).min().shift(-cycle)/df['close']
+    df.loc[(df['10天最低价']<df['10天最低价'][:duration,].quantile()),'Good sell Point?'] = 1 #6/18新改动在train group里分好坏
+    df=df.drop(columns=['10天最低价'])
+def buyjudge(df,cycle=10,duration=-180):
+    #ticker_df['Max'] = ticker_df['close'].rolling(window = cycle).max().shift(-cycle)
+    df['Good Buy Point?'] =0
+    df['10天最高价'] =df['close'].rolling(window =cycle).max().shift(-cycle)/df['close']
+    df.loc[(df['10天最高价']>df['10天最高价'].iloc[:duration,].quantile()),'Good Buy Point?'] = 1
+    df=df.drop(columns=['10天最高价'])
+def plot_buy(name,dfplot,stock,a=0.93,b=0.99,c=0.015):
+    for ratio in np.arange(a,b,c):
+        dfplot['Buy']=0
+        dfplot['BuyPrice']=0
+        dfplot.loc[(dfplot['GoodBuyProb']>dfplot['GoodBuyProb'].quantile(ratio)),'Buy'] = 1
+        dfplot.loc[(dfplot['Buy']==1),'BuyPrice'] = dfplot['close']
+        buyratio=round(100*dfplot['Buy'].sum()/len(dfplot['Buy']),2)
+        x=dfplot.index
+        y1=dfplot['close']
+        y2=dfplot['BuyPrice']
+        plt.plot(x, y1,'c',label='Price')
+        plt.plot(x, y2, 'o', ms=4.5, label='Buy Point')
+        plt.ylim([min(y1)*0.98, max(y1)*1.02])
+        plt.title(stock+'\n'+name+'\nBuy Ratio='+str(buyratio)+'%, '+'Threshold='+str(round(dfplot['GoodBuyProb'].quantile(ratio),3)))
+        #plt.figtext(0.35,0.3,'Buy Ratio='+str(buyratio)+'%' , fontsize=13)
+        plt.figtext(0.7,0.5,'Today:'+str(round(dfplot.iloc[-1,1],3)) , fontsize=13)
+        plt.figtext(0.7,0.45,'1 day:'+str(round(dfplot.iloc[-2,1],3)) , fontsize=13)
+        plt.figtext(0.7,0.4,'2 day:'+str(round(dfplot.iloc[-3,1],3)) , fontsize=13)
+        plt.legend(loc='upper left')
+        plt.show()
+
+def plot_sell(name,dfplot,stock,a=0.93,b=0.99,c=0.015):
+    for ratio in np.arange(a,b,c):
+        dfplot['sell']=0
+        dfplot['sellPrice']=0
+        dfplot.loc[(dfplot['GoodsellProb']>dfplot['GoodsellProb'].quantile(ratio)),'sell'] = 1
+        dfplot.loc[(dfplot['sell']==1),'sellPrice'] = dfplot['close']
+        sellratio=round(100*dfplot['sell'].sum()/len(dfplot['sell']),2)
+        x=dfplot.index
+        y1=dfplot['close']
+        y2=dfplot['sellPrice']
+        plt.plot(x, y1,'c',label='Price')
+        plt.plot(x, y2, 'o', ms=4.5, label='sell Point',color='blue')
+        plt.ylim([min(y1)*0.98, max(y1)*1.02])
+        plt.title(stock+'\n'+name+'\nsell Ratio='+str(sellratio)+'%, '+'Threshold='+str(round(dfplot['GoodsellProb'].quantile(ratio),3)))
+        #plt.figtext(0.35,0.3,'sell Ratio='+str(sellratio)+'%' , fontsize=13)
+        plt.figtext(0.7,0.5,'Today:'+str(round(dfplot.iloc[-1,1],3)) , fontsize=13)
+        plt.figtext(0.7,0.45,'1 day:'+str(round(dfplot.iloc[-2,1],3)), fontsize=13)
+        plt.figtext(0.7,0.4,'2 day:'+str(round(dfplot.iloc[-3,1],3)) , fontsize=13)
+        plt.legend(loc='upper left')
+        plt.show()
+
+
---
+++ A股买.py
@@ -0,0 +1,183 @@
+import pandas as pd
+import datetime
+import numpy as np
+import tushare as ts
+from array import *
+import pandas_datareader.data as web
+from pandas import Series, DataFrame
+import matplotlib.pyplot as plt
+import matplotlib
+from sklearn.model_selection import cross_val_score,cross_val_predict
+from sklearn.naive_bayes import GaussianNB
+from sklearn import linear_model, datasets
+from sklearn import svm
+from xgboost import XGBClassifier as Xgb
+from sklearn.model_selection import TimeSeriesSplit
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import precision_recall_curve
+from Afunction import buyjudge,stochastic_oscillator,plot_precision_recall_vs_threshold,plot_buy
+from sklearn import preprocessing
+from sklearn.ensemble import RandomForestClassifier   
+matplotlib.style.use('ggplot')
+#%%数据获取模块
+ts.set_token('fb60c870e18798f256a5d5dcd9faac6cb458ac9640144da99d998d90')
+pro = ts.pro_api()
+
+stock='601238.SH'#农行 601288.SH   
+df = pro.daily(ts_code=stock, start_date='20050101',end_date=datetime.date.today().strftime('%Y%m%d')).sort_index(axis=0, ascending=False)
+df['trade_date'] = pd.to_datetime(df['trade_date'])
+df=df.set_index(['trade_date'])
+
+df_HS300 = pro.index_daily(ts_code='000300.SH', start_date='20050101', end_date=datetime.date.today().strftime('%Y%m%d')).sort_index(axis=0, ascending=False).iloc[-len(df):,:]
+df_HS300['trade_date'] = pd.to_datetime(df_HS300['trade_date'])
+df_HS300=df_HS300.set_index(['trade_date'])
+# df_VIX = pro.index_daily(ts_code='000300.SH', start_date='20050101', end_date=datetime.date.today().strftime('%Y%m%d')).sort_index(axis=0, ascending=False).iloc[-len(df):,:]
+# df_VIX['trade_date'] = pd.to_datetime(df_VIX['trade_date'])
+# df_VIX=df_HS300.set_index(['trade_date'])
+testduration=-90
+#%%
+method_name = [{
+                'Random Forrest':RandomForestClassifier(),
+                'Random Forrest30':RandomForestClassifier(oob_score=True, random_state=30),
+                'Bayes(smo=1e-01)':GaussianNB(var_smoothing=1e-01),
+                'Bayes(smo=0.5)':GaussianNB(var_smoothing=0.5),
+                'Bayes(smo=1)':GaussianNB(var_smoothing=1),
+                'SVC(C=1)':svm.SVC(probability=True),
+                'SVC(linear, C=1)':svm.SVC(kernel='linear', C=1,probability=True),
+                'SVC(poly, C=1)':svm.SVC(kernel='poly',probability=True),
+                'XGBT(λ=1)':Xgb(reg_lambda=1),#Result of parameter tunning in XGBPara.py
+                'XGBT(λ=0.8)':Xgb(reg_lambda=0.8)
+                }]
+method_list=pd.DataFrame(method_name)
+ResultTable=DataFrame(columns=['Stock','Method','AvgScores','StdScores'])
+
+rawdata=df.iloc[testduration:]['close']
+#Add features in
+df['MAVOL150'] = df['vol']/df['vol'].rolling(150).mean()
+df['MAVOL20'] = df['vol']/df['vol'].rolling(20).mean()
+df['MAVOL10'] = df['vol']/df['vol'].rolling(10).mean()
+df['MAVOL5'] = df['vol']/df['vol'].rolling(5).mean()
+df['amount100'] = df['amount']/df['amount'].rolling(100).mean()
+df['amount20'] = df['amount']/df['amount'].rolling(20).mean()
+df['amount10'] = df['amount']/df['amount'].rolling(10).mean()
+df['amount5'] = df['amount']/df['amount'].rolling(5).mean()
+df['HS300'] = df_HS300['close']
+df['HS300_ROC'] = 100*df['HS300'].diff(1)/df['HS300'].shift(1)
+df['close_ROC'] = 100*df['close'].diff(1)/df['close'].shift(1)
+df['close/HS300'] = df['close_ROC']/df['close_ROC']  
+stochastic_oscillator(df)
+df['Intersection'] = 0
+df.loc[(df['K']>df['D']) & (df['K_prev']<df['D_prev']) & (df['D']<=80)  & (df['D_diff']>0),'Intersection'] = 1# Intersections: K go exceeds D   
+df['# Inter 10-day'] = df['Intersection'].rolling(14).sum()# number of intersections during past 10 days    
+df['close/MA10']= df['close']/df['close'].rolling(10).mean()# df['MA10']= df['close'].rolling(10) Moving Average of the past 10 days
+df['close/MA20']= df['close']/df['close'].rolling(20).mean()
+df['close/MA50']= df['close']/df['close'].rolling(50).mean()
+df['close/MA100']= df['close']/df['close'].rolling(100).mean()
+df['close/MA150']= df['close']/df['close'].rolling(150).mean()
+df['VAR5']= df['close_ROC'].rolling(5).std()
+df['VAR10']= df['close_ROC'].rolling(10).std()
+buyjudge(df,duration=testduration)
+featurelist=['amount100','amount20','amount10','amount5',
+             '# Inter 10-day','Intersection',
+             'close/HS300',
+             'MAVOL20','MAVOL10','MAVOL5','MAVOL150',
+            'close_ROC','rsv','K','D','J',
+            'K_ROC','D_ROC','K_diff','D_diff','J_ROC','J_diff','close/MA10',
+            'close/MA20','close/MA50','close/MA100',
+            'close/MA150',
+            'VAR5','VAR10']
+  
+df=df.drop(columns=['10天最高价'])
+df.dropna(axis=0, how='any', inplace=True)
+xshow=df.iloc[testduration:,:].loc[:,featurelist]
+xshow = preprocessing.MinMaxScaler().fit_transform(xshow)
+if None in xshow[-1,:]:
+    xshow=np.delete(xshow,-1,0)
+X=df.loc[:,featurelist]
+X = preprocessing.MinMaxScaler().fit_transform(X)
+
+y=df.loc[:,'Good Buy Point?']
+# Split train set and test set
+xtrain,ytrain=X[:testduration],y[:testduration]
+xtest,ytest=X[testduration:],y[testduration:]
+
+Market_GoodRatio=sum(df['Good Buy Point?'].iloc[:testduration,]==1)/len(df['Good Buy Point?'].iloc[:testduration,])#Good Buy Point Ratio in market is manully set to nearly 0.5 
+ResultTable=ResultTable.append({'Stock':stock,'Method':'Market Good Buy Ratio','AvgScores':Market_GoodRatio,'StdScores':0},ignore_index=True)
+
+#Compare and Plot the precision rate of each algorithm        
+index=0
+for method in method_list.loc[0,:]:
+    clf = method
+    cv=TimeSeriesSplit(n_splits=4) #Time series test
+    scores = cross_val_score(clf,xtrain, ytrain, cv=4,scoring='precision')
+    print(scores[scores>0])
+    series={'Stock':stock,'Method':method_list.columns[index],'AvgScores':scores[scores>0].mean(),'StdScores':scores[scores>0].std()}
+    index=index+1
+    ResultTable=ResultTable.append(series,ignore_index=True)
+
+name_list= ['Market Good Buy Ratio']
+name_list=np.append(name_list,method_list.columns)
+num_list= ResultTable.loc[ResultTable['Stock']==stock]['AvgScores']
+plt.barh(range(len(num_list)), num_list,tick_label = name_list)
+plt.title(stock+'\nPrecision Rate')
+plt.show()
+    
+#Plot precision rate of each method 
+index=0
+for method in method_list.loc[0,:]:
+     clf = method
+     clf.fit(xtrain, ytrain)
+     buypredicted = clf.predict_proba(xtest)
+     precision, recall, threshold = precision_recall_curve(ytest, buypredicted[:,1])
+     plot_precision_recall_vs_threshold(index,stock,method_list,precision, recall, threshold)
+     plt.show()
+     index=index+1
+#%%       Naive Bayes       
+clfbuy =GaussianNB(var_smoothing=1) 
+clfbuy.fit(xtrain, ytrain)
+buypredicted = clfbuy.predict_proba(xshow)    
+dfplot=pd.DataFrame()
+dfplot.loc[:,'close']=rawdata
+dfplot.loc[:,'GoodBuyProb']=buypredicted[:,1]
+plot_buy('Naive Bayes',dfplot,stock,0.9,1,0.03)
+#%%  SVM             
+clfbuy = svm.SVC(C=1,kernel='linear',probability=True)
+clfbuy.fit(xtrain, ytrain)
+buypredicted = clfbuy.predict_proba(xshow)    
+dfplot=pd.DataFrame()
+dfplot.loc[:,'close']=rawdata
+dfplot.loc[:,'GoodBuyProb']=buypredicted[:,1]
+plot_buy('SVM Linear',dfplot,stock,0.9,0.99,0.02)
+#%%  SVM             
+clfbuy = svm.SVC(C=1,probability=True)
+clfbuy.fit(xtrain, ytrain)
+buypredicted = clfbuy.predict_proba(xshow)    
+dfplot=pd.DataFrame()
+dfplot.loc[:,'close']=rawdata
+dfplot.loc[:,'GoodBuyProb']=buypredicted[:,1]
+plot_buy('SVM ',dfplot,stock,0.9,0.99,0.02)
+#%%  SVM Poly         
+clfbuy = svm.SVC(C=1,kernel='poly',probability=True)
+clfbuy.fit(xtrain, ytrain)
+buypredicted = clfbuy.predict_proba(xshow)    
+dfplot=pd.DataFrame()
+dfplot.loc[:,'close']=rawdata
+dfplot.loc[:,'GoodBuyProb']=buypredicted[:,1]
+plot_buy('SVM Poly',dfplot,stock,0.93,0.99,0.015)
+
+#%%  Random Forrest       
+clfbuy =RandomForestClassifier(oob_score=True, random_state=30)
+clfbuy.fit(xtrain, ytrain)
+buypredicted = clfbuy.predict_proba(xshow)    
+dfplot=pd.DataFrame()
+dfplot.loc[:,'close']=rawdata
+dfplot.loc[:,'GoodBuyProb']=buypredicted[:,1]
+plot_buy('Random Forrest',dfplot,stock,0.93,0.99,0.015)
+#%%       XGboost       
+clfbuy =Xgb(reg_lambda=1)
+clfbuy.fit(xtrain, ytrain)
+buypredicted = clfbuy.predict_proba(xshow)    
+dfplot=pd.DataFrame()
+dfplot.loc[:,'close']=rawdata
+dfplot.loc[:,'GoodBuyProb']=buypredicted[:,1]
+plot_buy('XGBoost',dfplot,stock,0.9,1,0.03)
